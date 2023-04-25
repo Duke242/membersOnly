8c6fc56c0e09b782242f4b7ff791b748392c4cd4
Duke <108301121+Duke242@users.noreply.github.com>
2023-04-25 02:09:58 +0000

    confirm password kinda working

--- app.js
+++ app.js
@@ -7,12 +7,72 @@ const signUpRouter = require('./routes/sign-up')
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 const models = require('./model/mongoose')
+const loginRouter = require('./routes/log-in') 
+const session = require("express-session");
+const passport = require("passport");
+const LocalStrategy = require("passport-local").Strategy;
 
 var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
+
+
+passport.use(
+  new LocalStrategy(async(username, password, done) => {
+    try {
+      const user = await User.findOne({ username: username });
+      if (!user) {
+        return done(null, false, { message: "Incorrect username" });
+      };
+      bcrypt.compare(password, user.password, (err, res) => {
+        if (res) {
+          // passwords match! log user in
+          return done(null, user)
+        } else {
+          // passwords do not match!
+          return done(null, false, { message: "Incorrect password" })
+        }
+      });
+      return done(null, user);
+    } catch(err) {
+      return done(err);
+    };
+  })
+);
+
+passport.serializeUser(function(user, done) {
+  done(null, user.id);
+});
+
+passport.deserializeUser(async function(id, done) {
+  try {
+    const user = await User.findById(id);
+    done(null, user);
+  } catch(err) {
+    done(err);
+  };
+});
+
+app.use(function(req, res, next) {
+  res.locals.currentUser = req.user;
+  next();
+});
+
+app.get("/log-out", (req, res, next) => {
+  req.logout(function (err) {
+    if (err) {
+      return next(err);
+    }
+    res.redirect("/");
+  });
+});
+
+app.use(session({ secret: "cats", resave: false, saveUninitialized: true }));
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(express.urlencoded({ extended: false }));
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -23,6 +83,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/sign-up', signUpRouter)
+app.use('/log-in', loginRouter)
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/sign-up.js
+++ routes/sign-up.js
@@ -1,25 +1,45 @@
-const express = require('express')
-const router = express.Router()
-const { setup } = require('../model/mongoose')
-const mongoose = require('mongoose')
+const express = require("express");
+const router = express.Router();
+const { setup } = require("../model/mongoose");
+const mongoose = require("mongoose");
+const bcrypt = require('bcryptjs')
+
+// const passwordInput = document.getElementById('firstPassword')
+// const confirmPassword = document.getElementById('confirmPassword')
 
 router.get("/", (req, res) => {
-  res.render('sign-up-form')
-})
+  res.render("sign-up-form");
+});
 
-router.post("/", async (req, res) => {
-  setup(mongoose)
-  const User = mongoose.model('user')
-  const { firstName, lastName, username, password } = req.body
-  const newUser = new User({ firstName, lastName, username, password, membershipStatus: "default" })
-  await newUser.save()
-  res.json("user created")
+router.post("/", async (req, res, next) => {
+  const { password, confirmPassword } = req.body
+  try {
+    bcrypt.hash(req.body.password, 10, async (err, hashedPassword) => {
+      if (err) {
+        console.log(err); 
+      }
+      else if (password !== confirmPassword) {
+          res.send("Passwords do not match")
+        }
+      else {
+      console.log(password, confirmPassword)
+      setup(mongoose);
+      const User = mongoose.model("user");
+      const { firstName, lastName, username } = req.body;
+      const newUser = new User({
+        firstName,
+        lastName,
+        username,
+        password: hashedPassword,
+        membershipStatus: "default",
+      });
+        await newUser.save();
+        res.redirect("/");
+      }
+    });
+  } catch (err) {
+    return next(err);
+  }
+});
 
-})
-
-
-
-
-
-
-module.exports = router 
+module.exports = router;
